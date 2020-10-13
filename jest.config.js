5375b0fcc51afc89868a0ac7db7b015cc2cd8454
Gayrat <gena68@inbox.ru>
2020-10-13 05:10:37 +0300

    собственный config работает

---
+++ jest.config.js
@@ -0,0 +1,28 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+    clearMocks: true,
+    coverageDirectory: "coverage",
+    testEnvironment: "jsdom",
+    setupFilesAfterEnv: ["<rootDir>/internals/jestSettings.js"],
+    transform: {
+        "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+        "^.+\\.mdx$": "@storybook/addon-docs/jest-transform-mdx",
+    },
+    moduleNameMapper: {
+        // https://jestjs.io/docs/en/webpack#handling-static-assets
+        "\\.(css|less)$": "<rootDir>/internals/__mocks__/styleMock.js",
+        "^@/(.*)$": "<rootDir>/src/$1",
+    },
+    testPathIgnorePatterns: ["e2e"],
+    moduleDirectories: ["node_modules", "src"],
+    coverageThreshold: {
+        global: {
+            branches: 85,
+            functions: 74,
+            lines: 85,
+            statements: -100,
+        },
+    },
+};
